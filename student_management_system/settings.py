7fff5da9db6e9f90f8bad14f145d918029119841
kavyp12 <92688681+kavyp12@users.noreply.github.com>
2025-05-04 19:57:09 +0530

    Add Whitenoise for static file serving

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -124,10 +124,10 @@
 # AUTH_USER_MODEL = 'app.CustomUser'
 
 
+
 import os
 from pathlib import Path
 from decouple import config
-import dj_database_url
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
@@ -166,7 +166,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'student_management_system.urls'
 
 TEMPLATES = [
@@ -187,21 +186,32 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'student_management_system.wsgi.application'
 
-# Database configuration using dj-database-url
+# Database
 DATABASES = {
-    'default': dj_database_url.config(
-        default=config('POSTGRES_URL'),
-        conn_max_age=600,
-        ssl_require=True  # Supabase requires SSL
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT', default='5432'),
+    }
 }
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
-    {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator'},
-    {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
 ]
 
 # Internationalization
@@ -215,7 +225,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files (for profile_pic)
 CLOUDINARY_STORAGE = {
