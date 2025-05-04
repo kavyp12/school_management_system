0c2718530dee07100d31fac26e725ffb4b82a054
kavyp12 <92688681+kavyp12@users.noreply.github.com>
2025-05-04 16:16:12 +0530

    Enable debug for troubleshooting

--- student_management_system/wsgi.py
+++ student_management_system/wsgi.py
@@ -1,16 +1,13 @@
 """
 WSGI config for student_management_system project.
 
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+It exposes the WSGI callable as a module-level variable named ``application`` and ``app`` for Vercel.
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'student_management_system.settings')
 
 application = get_wsgi_application()
+app = application  # Required for Vercel
