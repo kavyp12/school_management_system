ed183a227bbe1de9cf2f3cd2c93cfb60e00eab80
kavyp12 <92688681+kavyp12@users.noreply.github.com>
2025-05-04 19:19:26 +0530

    Add Whitenoise for static file serving

--- student_management_system/settings.py
+++ student_management_system/settings.py
@@ -158,6 +158,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this after SecurityMiddleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
